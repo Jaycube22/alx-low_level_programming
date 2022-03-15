67b528b80a57b4028e3dcc649c7340928887b6b6
Jaycube <adewolejesujoba@gmail.com>
2022-03-14 21:58:18 -0700

    i

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,23 +1,11 @@
 #include <stdio.h>
-
-
-
 /**
-
  * main - Entry point
-
  *
-
- * Return: Always 0 (Success)
-
+ * Return: 0 after printing the function
  */
-
 int main(void)
-
 {
-
-    return (0);
-
-
-
-clear
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
